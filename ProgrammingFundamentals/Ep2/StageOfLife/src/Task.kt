f0420cc5669defada06ab3a0a8fd9c067424eaab
Visaraswat <vikassaraswat0403@gmail.com>
2024-04-26 16:39:23 +0530

    Ep2 completed

--- ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
+++ ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
@@ -1,7 +1,13 @@
 import com.faangx.ktp.basics.StageOfLifeMiniApp
 
 fun stageOfLife(age: Int): String {
-    TODO()
+    when {
+        age >= 60 -> return "Old age"
+        age >= 18 -> return "Adulthood"
+        age >= 12 -> return "Teenage"
+        age >= 3 -> return "Childhood"
+        else -> return "Infancy"
+    }
 }
 
 fun main() {
